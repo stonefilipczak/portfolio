4aa8937df76ec1c91f30b6cf1f9c10eb5eb06450
Stone Filipczak <stonejacobs@gmail.com>
2019-08-23 14:34:23 -0400

    initial commit

---
+++ src/app.js
@@ -0,0 +1,67 @@
+const s = sketch => {
+  sketch.setup = () => {
+    sketch.createCanvas(sketch.windowWidth, 300);
+    sketch.frameRate(15);
+  };
+
+  sketch.draw = () => {
+    sketch.clear();
+
+    for (i = 0; i < sketch.height; i += 28) {
+      sketch.strokeWeight(sketch.random(2));
+      // sketch.stroke(sketch.random(210, 255), sketch.random(255), 100);
+      sketch.line(0, i, sketch.width, i);
+    }
+    // sketch.stroke(255);
+    // sketch.ellipse(sketch.width / 2, sketch.height / 2, 230);
+    sketch.stroke(0);
+    sketch.strokeWeight(20);
+
+    for (i = 0; i < sketch.height; i += 28) {
+      if (sketch.mouseY >= i) {
+        sketch.line(0, i, sketch.width, i);
+      }
+    }
+  };
+
+  sketch.windowResized = () => {
+    sketch.resizeCanvas(sketch.windowWidth, 300);
+  };
+};
+
+let myp5 = new p5(s, "bars");
+
+function aboutClick() {
+  window.scrollBy({
+    top: 450, // could be negative value
+    left: 0,
+    behavior: "smooth"
+  });
+  setTimeout(function() {
+    document.getElementById("about").classList.add("show");
+  }, 300);
+}
+
+function projectsClick() {
+  window.scrollBy({
+    top: 730, // could be negative value
+    left: 0,
+    behavior: "smooth"
+  });
+  Array.from(document.getElementsByClassName("piece")).forEach(function(
+    element,
+    index,
+    array
+  ) {
+    console.log(element);
+    element.classList.add("show");
+  });
+}
+
+function contactClick() {
+  window.scrollBy({
+    top: 4000, // could be negative value
+    left: 0,
+    behavior: "smooth"
+  });
+}
